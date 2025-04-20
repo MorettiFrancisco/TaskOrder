4f536230f0349dd4eccc303ab60ef853b6916956
morettiFrancisco <franmoretti02@hotmail.com>
2025-04-20 00:22:09 -0300

    Refactor TabLayout component to use dynamic colors for active and inactive tabs based on color scheme

--- fichero-cirugias/app/(tabs)/_layout.tsx
+++ fichero-cirugias/app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -12,16 +11,20 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  // Rosado fuerte en claro, rosado claro en oscuro
+  const activeColor = colorScheme === 'dark' ? '#ffb6d5' : '#d72660';
+  const inactiveColor = colorScheme === 'dark' ? '#aaa' : '#bbb';
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: activeColor,
+        tabBarInactiveTintColor: inactiveColor,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
           },
           default: {},
@@ -38,9 +41,9 @@ export default function TabLayout() {
         name="configuration"
         options={{
           title: 'Configuración',
-          tabBarIcon: ({ color }) => <AntDesign name="setting" size={28} color={color}/>,
+          tabBarIcon: ({ color }) => <AntDesign name="setting" size={28} color={color} />,
         }}
-        />;
+      />
     </Tabs>
   );
 }
